c0702c6da29e32d162b10a9d4bac7026c388d64a
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-04-16 19:29:20 +0900

    config

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,20 @@
-export const config = {
-  todoEntity: {
+export const entity = {
+  todo: {
     title: {
       length: 10,
     },
     body: {
       length: 30,
+    },
+  },
+};
+
+export const dto = {
+  task: {
+    input: {
+      body: {
+        ...entity.todo.body,
+      },
     },
   },
 };
